2eea1029f0dfdd506a057eb48e3b6d0972923884
Daniyal Alam <90774564+DaniyalAlam09@users.noreply.github.com>
2022-07-07 21:22:49 +0500

    Update Top.js

--- src/Components/Top.js
+++ src/Components/Top.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Top() {
+  const cities = [
+    { id: 1, name: "Lahore" },
+    { id: 2, name: "Karachi" },
+    { id: 3, name: "Islamabad" },
+    { id: 4, name: "Murree" },
+  ];
+
   return (
-    <div>Top</div>
-  )
+    <div className="flex justify-around items-center my-5">
+      {cities.map((city) => (
+        <button className="text-white text-lg font-medium" key={city.id}>
+          {city.name}
+        </button>
+    ))}
+    </div>
+  );
 }
 
-export default Top
+export default Top;
