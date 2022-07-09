c752142a570c0041292c4f3df7f8f045eb1c3ae7
Daniyal Alam <90774564+DaniyalAlam09@users.noreply.github.com>
2022-07-09 15:43:40 +0500

    Logics Done

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Input from "./Components/Input";
 import CityTIme from "./Components/CityTIme";
 import TemperatureDetails from "./Components/TemperatureDetails";
 import Forecast from "./Components/Forecast";
-import getWeather from "./Services/Api";
+import getFormattedWeather from "./Services/Api";
 
 function App() {
-  const fetchingData = async ()=>{
-    const data = await getWeather("weather", {q:"Lahore"})
-
-  }
-
+  const fetchingData = async () => {
+    const data = await getFormattedWeather({ q: "Lahore" });
+    console.log(data);
+  };
+  fetchingData();
 
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 shadow-xl shadow-gray-400">
@@ -20,8 +20,8 @@ function App() {
       <Input />
       <CityTIme />
       <TemperatureDetails />
-      <Forecast time="Daily Forcast"/>
-      <Forecast time="Hurly Forcast"/>
+      <Forecast time="Daily Forcast" />
+      <Forecast time="Hurly Forcast" />
     </div>
   );
 }
--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,32 +1,89 @@
+import { DateTime } from "luxon";
+
 const API_KEY = "2b16f29548a487aa37fdf466dba85d79";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeather = (info, params) => {
   const url = new URL(BASE_URL + "/" + info);
   url.search = new URLSearchParams({ ...params, appid: API_KEY });
-  return fetch(url).then((response) =>response.json());
+  return fetch(url).then((response) => response.json());
 };
 
+const formatedCurrentWeather = (data) => {
+  const {
+    coord: { lon, lat },
+    main: { temp, feels_like, temp_min, temp_max, humidity },
+    wind: { speed },
+    sys: { country, sunrise, sunset },
+    name,
+    dt,
+    weather,
+  } = data;
+  const { main: details, icon } = weather[0];
 
-const formatedCurrentWeather =(data)=>{
-    const  {
-        coord:{lon,lat},
-        main:{temp,feels_like,temp_min,temp_max,humidity},
-        wind:{speed},
-        sys:{country, sunrise,sunset},
-        name,
-        dt,
-        weather,
+  return {
+    lat,
+    lon,
+    temp,
+    feels_like,
+    temp_min,
+    temp_max,
+    humidity,
+    speed,
+    country,
+    sunrise,
+    sunset,
+    name,
+    dt,
+    details,
+    icon,
+  };
+};
 
-        } = data
-        const  {main: details, icon} = weather[0];
+const formatedForecastWeather = (data) => {
+  let { timezone, daily, hourly } = data;
+  daily = daily.slice(1, 6).map((d) => {
+    return {
+      title: formatLocalTime(d.dt, timezone, "ccc"),
+      temp: d.temp.day,
+      icon: d.weather[0].icon,
+    };
+  });
+  hourly = hourly.slice(1, 6).map((d) => {
+    return {
+      title: formatLocalTime(d.dt, timezone, "hh:mm a"),
+      temp: d.temp,
+      icon: d.weather[0].icon,
+    };
+  });
+  return { timezone, daily, hourly };
+};
 
-        return{
-            lat,lon,temp,feels_like,temp_min,temp_max,humidity,speed,country, sunrise,sunset,name,dt,details, icon
-        }
-}
+const getFormattedWeather = async (params) => {
+  const formatCurrentWeather = await getWeather("weather", params).then(
+    formatedCurrentWeather
+  );
+  const { lat, lon } = formatCurrentWeather;
+  const formatForcastWeather = await getWeather("onecall", {
+    lat,
+    lon,
+    exclude: "current,minutely,alerts",
+    units: params.units,
+  }).then(formatedForecastWeather);
+  return { ...formatCurrentWeather, ...formatForcastWeather };
+};
 
-const getFormattedWeather =async(params)=>{
-    const formatCurrentWeather =await getWeather('weather', params).then(formatedCurrentWeather)
-    return formatCurrentWeather
-}
+const formatLocalTime = (
+  secs,
+  zone,
+  format = "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => {
+  DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+};
+
+const iconUrl = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+export default getFormattedWeather;
+
+export {formatLocalTime, iconUrl}
